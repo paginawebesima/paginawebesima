7dfe4f68d19ac207984624b4e52f41c9fe7ceffc
MARCO-ROSAS\markr <marco.rosas.isw@unipolidgo.edu.mx>
2024-05-19 21:51:01 -0600

    First Commit

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,23 +1,47 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import { useState } from 'react';
 
 export default function Login() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <>
-        <header className="">
-        <NavBar/>
+      <header className="">
+        <NavBar />
         <div className="imagen2 clase1">
-            <h1></h1>
-            <h3 className="texto-principal">Bienvenido<br /> Inicia Sesion para continuar</h3>
+          <h1></h1>
+          <h3 className="texto-principal">¡Bienvenido!<br />Inicia Sesión para continuar</h3>
         </div>
-        </header>
-        <div>
-            <form action="">
-                <h1>Iniciar Sesion</h1>
-                <input type="text" placeholder="usuario" />
-                <input type="password" placeholder="contraseña" />
-                <input type="submit" value='Iniciar Sesion' />
-            </form>
+      </header>
+      <div className="login-container">
+        <div className="image-container">
+          <img src="EsimaLogo.png" alt="Logo ESIMA" className="logo-image" />
         </div>
+        <div className="form-container">
+          <form action="">
+            <input type="text" className="input mail" placeholder="Correo" />
+            <div className="password-input">
+              <input
+                type={passwordVisible ? "text" : "password"}
+                className="input password"
+                placeholder="Contraseña"
+              />
+              <img
+                src={passwordVisible ? "eye-lock-login.svg" : "eye-login.svg"}
+                alt="Mostrar Contraseña"
+                className="password-toggle-icon"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+            <input type="submit" className="button" value='Ingresar' />
+          </form>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
