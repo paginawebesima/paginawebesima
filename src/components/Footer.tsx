48f1f0416260b9cf4932a25f4ee6178ac637db0f
MARCO-ROSAS\markr <marco.rosas.isw@unipolidgo.edu.mx>
2024-05-19 21:39:16 -0600

    First Commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,39 @@
 export default function Footer() {
   return (
     <>
-        <div className="footer">
-            <p className="texto-centrado">Escuela Secundaria Ignacio Manuel Altamirano</p>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-1">
+              <ul className="footer-list">
+                <li className="footer-item">
+                  <a href="#">
+                    <span className="footer-icon"><img src="location.svg" alt="location icon" /></span>
+                    16°48'10, C. Cap. Francisco de Ibarra 1907 OTE-95°05'57, Nueva Vizcaya, 34080 Durango, Dgo.
+                  </a>
+                </li>
+                <li className="footer-item">
+                  <span className="footer-icon"><img src="phone.svg" alt="phone icon" /></span>
+                  618 530 3345
+                </li>
+                <li className="footer-item">
+                  <span className="footer-icon"><img src="mail.svg" alt="mail icon" /></span>
+                  direccionesima@hotmail.com
+                </li>
+                <li className="footer-item">
+                  <a href="https://www.facebook.com/ESIMAof/">
+                    <span className="footer-icon"><img src="facebook.svg" alt="facebook icon" /></span>
+                    https://www.facebook.com/ESIMAof/
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <div className="copyright">
+          <p>&copy; 2024 ESIMA. Todos los derechos reservados.</p>
+        </div>
+      </footer>
     </>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 export default function NavBar() {
+  const location = useLocation();
   return (
     <>
-        <nav className="header clase2">
-            <Link to='/'><img className="" src="LogoEsima.png" alt="Esima" /></Link>
-            <Link className="enlace " to='/preinscripciones'>Preincipciones</Link>
-            <Link className="enlace " to='/talleres'>Talleres</Link>
-            <Link className="enlace " to='/graduaciones'>Graduaciones</Link>
-            <Link className="enlace " to='/administrativos'>Administrativos</Link>
-            <Link className="enlace" to='/login'>Iniciar Sesion</Link>
-        </nav>
+      <nav className="header clase2">
+        <Link to='/'><img className="logo" src="LogoEsima.png" alt="Esima" /></Link>
+        <Link className={`enlace ${location.pathname === '/preinscripciones' ? 'active' : ''}`} to="/preinscripciones">Preinscripciones</Link>
+        <Link className={`enlace ${location.pathname === '/talleres' ? 'active' : ''}`} to="/talleres">Talleres</Link>
+        <Link className={`enlace ${location.pathname === '/graduaciones' ? 'active' : ''}`} to="/graduaciones">Graduaciones</Link>
+        <Link className={`enlace ${location.pathname === '/administrativos' ? 'active' : ''}`} to="/administrativos">Administrativos</Link>
+        <Link className={`enlace ${location.pathname === '/login' ? 'active' : ''}`} to="/login">Iniciar Sesión</Link>
+      </nav>
     </>
   )
 }
