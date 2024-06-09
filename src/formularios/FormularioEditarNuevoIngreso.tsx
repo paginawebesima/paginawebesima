fdb98c33b1814cf4e83d131ffb93c8e6ef2b5ec9
paginawebesima <paginawebesima@gmail.com>
2024-06-08 18:31:34 -0600

    Update FormularioEditarNuevoIngreso.tsx

--- src/formularios/FormularioEditarNuevoIngreso.tsx
+++ src/formularios/FormularioEditarNuevoIngreso.tsx
@@ -5,7 +5,7 @@ type FormularioEditarNuevoIngresoProps={
     register:UseFormRegister<PreinscripcionesEsima>
     errors:FieldErrors<PreinscripcionesEsima>
 }
-export default function FormularioEditarNuevoIngreso({errors,register}:FormularioEditarNuevoIngresoProps) {
+export default function FormularioEditarNuevoIngreso({register}:FormularioEditarNuevoIngresoProps) {
   return (
     <div className="form-register">
         <label htmlFor="">Titulo Requerimiento</label>
