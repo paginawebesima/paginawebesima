e18914690a5ba8d5e6fb2aade7cf3f4e65bc20ca
trejo903 <paginawebesima@gmail.com>
2024-07-08 17:26:41 -0600

    08 de julio

--- src/views/usuarios/EditarUsuarios.tsx
+++ src/views/usuarios/EditarUsuarios.tsx
@@ -1,56 +1,56 @@
-import { ToastContainer, toast } from 'react-toastify';
-import { Link} from "react-router-dom"
-import { User, UserRegistrationForm } from '../../types'; 
-import { useForm } from "react-hook-form"
-import { useMutation } from "react-query"
-import { actualizarUsuarios } from '../../api/api'; 
+// import { ToastContainer, toast } from 'react-toastify';
+// import { Link} from "react-router-dom"
+// import { User, UserRegistrationForm } from '../../types'; 
+// import { useForm } from "react-hook-form"
+// import { useMutation } from "react-query"
+// import { actualizarUsuarios } from '../../api/api'; 
 
 
-type EditarUsuariosProps={
-    data:UserRegistrationForm
-    prestamoId:User['_id']
-}
-export default function EditarUsuarios({data,usuariosId}:EditarUsuariosProps) {
-    const {register,handleSubmit,formState:{errors}}=useForm({defaultValues:{
-            name:data.name,
-            email:data.email,
-            rol:data.rol
-        }
-    })
-    const {mutate} = useMutation({
-        mutationFn:actualizarUsuarios,
-        onError:(error:Error)=>{
-            toast.error(error.message)
-        },
-        onSuccess:(data)=>{
-            toast.success(data)
+// type EditarUsuariosProps={
+//     data:UserRegistrationForm
+//     usuariosId:User['_id']
+// }
+// export default function EditarUsuarios({data,usuariosId}:EditarUsuariosProps) {
+//     const {register,handleSubmit,formState:{errors}}=useForm({defaultValues:{
+//             name:data.name,
+//             email:data.email,
+//             rol:data.rol
+//         }
+//     })
+//     const {mutate} = useMutation({
+//         mutationFn:actualizarUsuarios,
+//         onError:(error:Error)=>{
+//             toast.error(error.message)
+//         },
+//         onSuccess:(data)=>{
+//             toast.success(data)
             
-        }
-    })
-    const handleForm = (formData2:UserRegistrationForm)=>{
-        const data2 = {
-            formData2,
-            usuariosId
-        }
-        mutate(data2)
-    }
+//         }
+//     })
+//     const handleForm = (formData:UserRegistrationForm)=>{
+//         const data2 = {
+//             formData,
+//             usuariosId
+//         }
+//         mutate(data2)
+//     }
    
-  return (
-    <div>
-         <ToastContainer 
-         pauseOnHover={false}
-            pauseOnFocusLoss={false}
-        />
-        <h2 className='texto_prestamo'>Actualizar usuarios</h2>
-        <div className='div_formulario'>
-        <form className='formulario' noValidate onSubmit={handleSubmit(handleForm)}>
-            <User register={register} errors={errors}/>
-            <div className='formulario_flex'>
-            </div>
-            <input className='boton_guardar' type="submit" value='Guardar' />
-        </form>
-        </div>
-        <Link className="boton_regresar enlace_eliminar" to='/usuarios'>Regresar</Link>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//          <ToastContainer 
+//          pauseOnHover={false}
+//             pauseOnFocusLoss={false}
+//         />
+//         <h2 className='texto_prestamo'>Actualizar usuarios</h2>
+//         <div className='div_formulario'>
+//         <form className='formulario' noValidate onSubmit={handleSubmit(handleForm)}>
+//             <User register={register} errors={errors}/>
+//             <div className='formulario_flex'>
+//             </div>
+//             <input className='boton_guardar' type="submit" value='Guardar' />
+//         </form>
+//         </div>
+//         <Link className="boton_regresar enlace_eliminar" to='/usuarios'>Regresar</Link>
+//     </div>
+//   )
+// }
