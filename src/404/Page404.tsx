b58f0576ac3d4e28695485aeb1d683cc687f8ceb
trejo903 <paginawebesima@gmail.com>
2024-07-10 10:45:02 -0600

    Commit 10 de julio 10:44am

--- src/404/Page404.tsx
+++ src/404/Page404.tsx
@@ -7,12 +7,14 @@ export default function Page404() {
     <div>
     <header className="">
         <NavBar />
-        <div className="imagen10 clase1">
+        <div className="imagen20 clase1">
           <h1></h1>
           <h3 className="texto-principal">Pagina 404 <br/> Esta pagina no existe </h3>
         </div>
     </header>
+    <div className="div_boton404">
     <Link className="boton404" to='/'>Volver al inicio</Link>
+    </div>
     <Footer/>
     </div>
   )
