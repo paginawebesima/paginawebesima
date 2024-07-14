6d441a1d9d5a2e78abae1bcae4f02f1e85f21a6b
MARCO-ROSAS\markr <marco.rosas.isw@unipolidgo.edu.mx>
2024-07-14 01:40:25 -0600

    Commit 14 Julio

--- src/views/Administrativos.tsx
+++ src/views/Administrativos.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useQuery } from "react-query";
@@ -10,42 +9,7 @@ export default function Administrativos() {
     queryKey: ['administrativos'],
     queryFn: obtenerAdministrativos
   })
-  const administrativosRef = useRef<HTMLDivElement[]>([]);
-  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("down");
-  const previousScroll = useRef<number>(window.scrollY);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScroll = window.scrollY;
-
-      if (currentScroll < previousScroll.current) {
-        setScrollDirection("up");
-      } else {
-        setScrollDirection("down");
-      }
-
-      previousScroll.current = currentScroll;
-
-      administrativosRef.current.forEach((administrativo, index) => {
-        if (!administrativo) return;
-        if (index === 0) return; // Skip the first element
-
-        const top = administrativo.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-        if (top < windowHeight * 0.8 && scrollDirection === "down") {
-          administrativo.classList.add("aparecer");
-        } else if (top >= windowHeight * 0.8 && scrollDirection === "up") {
-          administrativo.classList.remove("aparecer");
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrollDirection]);
-
+ 
   return (
     <>
       <header className="">
